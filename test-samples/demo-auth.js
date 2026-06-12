427ab8b19cc196e7fa208dd65ac6522c6d831d01
ertaneker <ertaneker@gmail.com>
2026-06-12 15:27:00 +0300

    test: demo auth file for PR Shield testing

---
+++ test-samples/demo-auth.js
@@ -0,0 +1,14 @@
+﻿// Sample auth file for PR Shield demo
+const express = require("express");
+const app = express();
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const query = "SELECT * FROM users WHERE email = '" + email + "'";
+  db.query(query, (err, user) => {
+    req.session.user = user;
+    res.json({ token: "session-" + user.id });
+  });
+});
+
+module.exports = app;
