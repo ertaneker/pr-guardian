2578929b428d668c30afd758882f7147bfeb05a9
ertaneker <ertaneker@gmail.com>
2026-06-12 17:11:50 +0300

    feat: add phone field to User model

---
+++ prisma/migrations/20260101_init.sql
@@ -0,0 +1,8 @@
+﻿-- This is an old migration, not the one for the phone field
+-- Migration: 20260101_init
+
+CREATE TABLE "User" (
+  "id" SERIAL PRIMARY KEY,
+  "email" TEXT NOT NULL UNIQUE,
+  "name" TEXT
+);
